38c633b2f3fb30929c1e418a643dafe8b5ed4467
sarasampaiof <sarasamp1512@gmail.com>
2026-04-03 17:56:31 -0300

    Create antecessor_e_sucessor.py

---
+++ antecessor_e_sucessor.py
@@ -0,0 +1,6 @@
+numero = int(input("Insira um número inteiro: "))
+
+antecessor = numero - 1
+sucessor = numero + 1
+
+print(f"O antecessor e sucessor de {numero} são, respectivamente, {antecessor} e {sucessor}")
