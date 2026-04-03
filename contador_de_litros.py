491c70af51b34e1e1e6fb31e0bc7d222d0fae251
sarasampaiof <sarasamp1512@gmail.com>
2026-04-03 00:58:29 -0300

    Create contador_de_litros.py

---
+++ contador_de_litros.py
@@ -0,0 +1,7 @@
+lata_350ml = int(input("Informe a quantidade de latas de 350ml compradas: "))
+garrafa_600ml = int(input("Informe a quantidade de garrafas de 600ml compradas: "))
+garrafa_2L = int(input("Informe a quantidade de garrafas de 2 litros compradas: "))
+
+total_de_litros = (lata_350ml * 350 + garrafa_600ml * 600 + garrafa_2L * 2000) / 1000
+
+print("O total de litros comprados é: ", total_de_litros)
