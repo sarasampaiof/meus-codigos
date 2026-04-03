0a0239260a33e11d9146175cd498d67ed6fba162
sarasampaiof <sarasamp1512@gmail.com>
2026-04-03 01:42:23 -0300

    Update contador_de_litros.py

--- contador_de_litros.py
+++ contador_de_litros.py
@@ -2,6 +2,6 @@ lata_350ml = int(input("Informe a quantidade de latas de 350ml compradas: "))
 garrafa_600ml = int(input("Informe a quantidade de garrafas de 600ml compradas: "))
 garrafa_2L = int(input("Informe a quantidade de garrafas de 2 litros compradas: "))
 
-total_de_litros = (lata_350ml * 350 + garrafa_600ml * 600 + garrafa_2L * 2000) / 1000
+total_de_litros = (lata_350ml * 350 + garrafa_600ml * 600 + garrafa_2L * 2000) / 1000 #calculado em mls para passar para litros ao dividir por mil depois
 
 print("O total de litros comprados é: ", total_de_litros)
