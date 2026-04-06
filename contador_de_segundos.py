1f838548d0a2f6f0e9e275903bcc3fc94260baf3
sarasampaiof <sarasamp1512@gmail.com>
2026-04-06 00:20:17 -0300

    Update contador_de_segundos.py

--- contador_de_segundos.py
+++ contador_de_segundos.py
@@ -2,7 +2,6 @@ horas = int(input("Informe a quantidade de horas: ")) #horas * 3600 = segundos
 minutos = int(input("Informe a quantidade de minutos: ")) #minutos * 60 = segundos
 segundos = int(input("Informe a quantidade de segundos: "))
 
-
 total_de_segundos = (horas * 3600) + (minutos * 60) + segundos
 
 print("O total de segundos é ", total_de_segundos)
