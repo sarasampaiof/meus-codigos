59a9360e4e9b65ba08deed2c7a4c6839123de9c7
sarasampaiof <sarasamp1512@gmail.com>
2026-04-08 20:58:01 -0300

    Create gorjeta.py

---
+++ gorjeta.py
@@ -0,0 +1,7 @@
+valor_gasto = float(input("Informe o valor gasto no restaurante: "))
+
+gorjeta = valor_gasto * 0.1
+
+valor_total = valor_gasto + gorjeta
+
+print(f"O valor da gorjeta é {gorjeta:.2f} e o valor total a ser pago é {valor_total:.2f}")
