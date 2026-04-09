b2d4866c57df935b0cc1aeb5ea06ff7cfaa9a522
sarasampaiof <sarasamp1512@gmail.com>
2026-04-08 21:21:15 -0300

    Create aux.py

---
+++ aux.py
@@ -0,0 +1,8 @@
+num1 = int(input("Insira um número inteiro: "))
+num2 = int(input("Insira outro número inteiro: "))
+
+aux = num1
+num1 = num2
+num2 = aux
+
+print(f"O primeiro número agora é {num1} e o segundo é {num2}")
