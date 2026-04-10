5b050e412468c9d52ae95a8db393793b16dfa684
sarasampaiof <sarasamp1512@gmail.com>
2026-04-09 21:42:06 -0300

    Create ordem_crescente.py

---
+++ ordem_crescente.py
@@ -0,0 +1,7 @@
+num1 = int(input("Informe um primeiro número: "))
+num2 = int(input("Informe um segundo número: "))
+
+if num1 < num2:
+    print(f"Ordem: {num1}, {num2}")
+else:
+    print((f"Ordem: {num2}, {num1}"))
