7eef347606fcc290e4cff2950027f4c1267480f9
sarasampaiof <sarasamp1512@gmail.com>
2026-04-09 22:25:03 -0300

    Create num_compreendido.py

---
+++ num_compreendido.py
@@ -0,0 +1,6 @@
+numero = int(input("Informe um número: "))
+
+if numero >= 20 and numero <= 90:
+    print("Está compreendido entre 20 e 90")
+else:
+    print("Não está compreendido entre 20 e 90")
