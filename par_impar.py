7018abd3e6ea32d1bef0977c04681625f5f07c2c
sarasampaiof <sarasamp1512@gmail.com>
2026-04-16 21:10:21 -0300

    Update and rename par_impar.py to par_impar_modulo.py

--- par_impar.py
+++
@@ -1,6 +0,0 @@
-numero = int(input("Informe um número: "))
-
-if numero >= 0 and numero % 2 == 0:
-    print(f"{numero} é um número PAR.")
-elif numero >=0 and numero % 2 != 0:
-    print(f"{numero} é um número ÍMPAR.")
