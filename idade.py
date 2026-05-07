22a09f8b51273a842c3d1d449c2199d14cdbf213
sarasampaiof <sarasamp1512@gmail.com>
2026-05-07 14:56:37 -0300

    Create idade.py

---
+++ idade.py
@@ -0,0 +1,7 @@
+idade = int(input("Informe a idade do indivíduo: "))
+if idade < 18:
+    print("O indivíduo é menor de idade.")
+elif idade <= 64 :
+    print("O indivíduo é maior de idade.")
+else:
+    print("O indivíduo é da terceira idade.")
