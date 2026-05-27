6348366d269ad5d04457d3daa1a7ca898b093de2
Sara Sampaio Figueredo <sarasamp1512@gmail.com>
2026-05-27 01:08:56 -0300

    Create soma_nao_zero.py

---
+++ soma_nao_zero.py
@@ -0,0 +1,8 @@
+numero = int(input("Insira número: "))
+soma = 0
+
+while numero != 0:
+    soma += numero
+    numero = int(input("Insira número: "))
+    
+print(f"A soma dos números digitados é: {soma}")
